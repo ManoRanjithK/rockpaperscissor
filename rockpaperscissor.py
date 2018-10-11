ed51733c21ac8e693123b4e350e1b655e68095e9
Raj Kumar <rajkumar.cind@gmail.com>
2018-10-11 19:01:44 +0530

    Printing Changes to Results :-)

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -16,37 +16,37 @@ while player == False:
     if player == "Rock":
         if computer == "Paper":
             computerWon += 1
-            print("computer Won ",player, "by", computer)
+            print("Sorry Buddy,The Computer Won ",player, "by", computer)
         elif computer == "Rock":
             print("tie")
             tie += 1
         else:
             playerWon += 1
-            print("player won ", computer, "by", player)
+            print("Congratulations, Player won ", computer, "by", player)
     elif player == "Paper":
         if computer == "Siccosor":
             computerWon += 1
-            print("computer Won ",player, "by", computer)
+            print("Your Bad Luck, Computer Won ",player, "by", computer)
         elif computer == "Paper":
             print("tie")
             tie += 1
         else:
             playerWon += 1
-            print("player won ", computer, "by", player)
+            print("Im the king of the world, Player won ", computer, "by", player)
     elif player == "siccosor":
         if computer == "Rock":
             computerWon += 1
-            print("computer Won ", player, "by", computer)
+            print("Im gonna take over the world, Computer Won ", player, "by", computer)
         elif computer == "Siccosor":
             print("tie")
             tie += 1
         else:
             playerWon += 1
-            print("player won ", computer, "by", player)
+            print("Guess You Ha Ha, Player won ", computer, "by", player)
     elif player == "exit":
         break
     else:
-            print("invalid number only 0 or 1 or 2 else GET LOST")
+            print("Invalid number only 0 or 1 or 2 else GET LOST")
     player = False
     computer = items[r.randint(0, 2)]
 
